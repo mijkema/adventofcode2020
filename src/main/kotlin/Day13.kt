99cca9a3e9afe73d34146b0b709d66617f3b05da
Matthijs IJkema <matthijs.ijkema@teampicnic.com>
2020-12-13 09:10:55 +0100

    Solution Day 13.

---
+++ src/main/kotlin/Day13.kt
@@ -0,0 +1,42 @@
+package main
+
+import java.io.File
+
+fun main() {
+    val lines = File("inputs/Day13").readLines()
+    val startingTime = lines[0].toInt()
+    val busDepartureTimes = lines[1].split(',')
+
+    var waitingTime = Int.MAX_VALUE
+    var busTime = 0
+    busDepartureTimes.forEach {
+        if (it == "x") return@forEach
+        val schedule = it.toInt()
+        var current = schedule - startingTime.rem(schedule)
+        if (current < waitingTime) {
+            waitingTime = current
+            busTime = schedule
+        }
+    }
+    println("shortest: $busTime ($waitingTime), result: ${busTime * waitingTime}")
+
+    // part 2
+    var busTimes = mutableListOf<Pair<Int, Int>>()
+    busDepartureTimes.forEachIndexed{ i, s ->
+        if (s == "x") return@forEachIndexed
+        val schedule = s.toInt()
+        busTimes.add(Pair(i, schedule))
+    }
+
+    var position = 0L
+    var offset = 1L
+    busTimes.forEach {
+        var index = it.first
+        var schedule = it.second
+        while ((position + index).rem(schedule) != 0L) {
+            position += offset
+        }
+        offset *= schedule
+    }
+    println("solution part 2: $position")
+}
