62afb59709bd8215c525f1b830a4a05956e150fd
Matthijs IJkema <matthijs.ijkema@teampicnic.com>
2020-12-22 20:14:26 +0100

    Cleanup.

--- src/main/kotlin/Day22.kt
+++ src/main/kotlin/Day22.kt
@@ -33,36 +33,29 @@ fun main() {
         .map { e -> e.second.lines().filter(String::isNotEmpty).map(String::toInt).toMutableList() }
 
     // play
-    val cache = mutableMapOf<MutableList<Int>, Pair<Int, MutableList<Int>>>()
-    val winner = playRounds(players, cache)
+    val winner = playRounds(players)
 
     solution = winner.second.reversed().mapIndexed { i, v -> (i + 1) * v }.sum()
 
     println("solution: $solution")
 }
 
-fun playRounds(players: List<MutableList<Int>>, known: MutableMap<MutableList<Int>, Pair<Int, MutableList<Int>>>, depth: Int = 0): Pair<Int, MutableList<Int>> {
-    if (known.contains(players[0])) {
-        return known[players[0]]!!
-    }
-    val seen = mutableListOf<Pair<List<Int>, List<Int>>>()
+fun playRounds(players: List<MutableList<Int>>): Pair<Int, MutableList<Int>> {
+    val seen = mutableListOf<List<Int>>()
     var player1Wins = false
     loop@ while (!players.any { p -> p.isEmpty() }) {
-        if (seen.contains(Pair(players[0].toList(), players[1].toList()))) {
+        if (seen.contains(players[0].toList())) {
             player1Wins = true
-            known[players[0]] = Pair(0, players[0])
             break@loop
         }
-        seen.add(Pair(players[0].toList(), players[1].toList()))
+        seen.add(players[0].toList())
         val first = players[0].removeFirst()
         val second = players[1].removeFirst()
         if (players[0].size >= first && players[1].size >= second) {
             val newGame = players.map { l -> l.toMutableList() }.toMutableList()
             newGame[0] = newGame[0].subList(0, first)
             newGame[1] = newGame[1].subList(0, second)
-            val forCache = players[0].toMutableList()
-            val recursiveResult = playRounds(newGame, known, depth + 1)
-            known[forCache] = recursiveResult
+            val recursiveResult = playRounds(newGame)
             if (recursiveResult.first == 0) {
                 players[0].add(first)
                 players[0].add(second)
