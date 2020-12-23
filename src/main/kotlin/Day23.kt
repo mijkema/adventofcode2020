7b1df8aed2ab8c6fa422736c1e4c8b668f26a6aa
Matthijs IJkema <matthijs.ijkema@teampicnic.com>
2020-12-23 13:39:45 +0100

    Cleanup.

--- src/main/kotlin/Day23.kt
+++ src/main/kotlin/Day23.kt
@@ -2,24 +2,8 @@ package main
 
 fun main() {
     val input = "942387615".chunked(1).map{c -> c.toInt()}.toMutableList()
-    var highest = input.maxOrNull()!!
-    var map = input.mapIndexed{ i, v -> v to input[(i + 1) % highest]  }.toMap().toMutableMap()
+    var map = play(input, 100)
 
-    var current = input.first()
-    (1..100).forEach{
-        val x = map[current]!!
-        val y = map[x]!!
-        val z = map[y]!!
-        var dest = if (current - 1 < 1) highest else current - 1
-        while (dest == x || dest == y || dest == z) {
-            dest = if (dest - 1 < 1) highest else dest - 1
-        }
-        val afterDest = map[dest]!!
-        map[current] = map[z]!!
-        map[dest] = x
-        map[z] = afterDest
-        current = map[current]!!
-    }
     var solution = "" + map[1]
     var next = map[map[1]]
     while (next != 1) {
@@ -29,12 +13,16 @@ fun main() {
     println("solution: $solution")
 
     (10..1000000).forEach(input::add)
-    highest = input.maxOrNull()!!
-    map = input.mapIndexed{ i, v -> v to input[(i + 1) % highest]  }.toMap().toMutableMap()
+    map = play(input, 10000000)
+    println("solution: ${map[1]!!.toLong()*map[map[1]]!!.toLong()}")
+}
 
-    println(map.size)
-    current = input.first()
-    (1..10000000).forEach{
+fun play(input: MutableList<Int>, rounds: Int): Map<Int, Int> {
+    val highest = input.maxOrNull()!!
+    val map = input.mapIndexed{ i, v -> v to input[(i + 1) % highest]  }.toMap().toMutableMap()
+
+    var current = input.first()
+    (1..rounds).forEach{
         val x = map[current]!!
         val y = map[x]!!
         val z = map[y]!!
@@ -47,12 +35,6 @@ fun main() {
         map[dest] = x
         map[z] = afterDest
         current = map[current]!!
-        if (it % 1000000 == 0) {
-            println(it)
-        }
     }
-    val i1 = map[1]!!
-    val i2 = map[i1]!!
-    println("solution: $i1 * $i2 = ${i1.toLong()*i2.toLong()}")
-
+    return map
 }
